2b24c1e969bc21270248efcd1694716622a8c5ef
aidaa001 <aubakiraida01@gmail.com>
2025-07-16 01:38:22 +0500

    переместила файлы

--- styles/main.js
+++
@@ -1,7 +0,0 @@
-
-  const burger = document.getElementById('burger');
-  const menuLinks = document.getElementById('menuLinks');
-
-  burger.addEventListener('click', () => {
-    menuLinks.classList.toggle('show');
-  });
